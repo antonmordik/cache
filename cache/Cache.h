aa2ccef9bbf34926fa62d57a4a9340d1890025e6
Anton Mordik <mordikk@gmail.com>
2020-04-25 17:40:57 +0300

    modified cache model & added memory class

--- cache/Cache.h
+++ cache/Cache.h
@@ -1,24 +1,51 @@
 #include <string>
+#include "Memory.h"
 #pragma once
 
-enum CacheType
+enum CacheLineFlag {
+  DEFAULT = 0, DITRY = 1
+};
+
+struct CacheLine
 {
-  L1 = 1, L2 = 2
+  std::string tag;
+  std::string data;
+  CacheLineFlag flag;
+  int count;
+
+  CacheLine(std::string tag, std::string data) {
+    this->tag = tag;
+    this->data = data;
+    this->flag = CacheLineFlag::DEFAULT;
+    this->count = 0;
+  }
+
+  CacheLine() {
+    this->count = 0;
+    this->flag = CacheLineFlag::DEFAULT;
+  }
 };
 
 class Cache
 {
 public:
-	Cache(CacheType type);
+	Cache();
 	~Cache();
   std::string toString();
+
+  int push(std::string data); // adding line to cache
+  CacheLine pop(int index); // removing line from cache by index
+  CacheLine pop(std::string data); // removing line from cache by data
+  void clear(); // clearing cache method
+  void transferToMemory(CacheLine line, Memory* memory);
+
 private:
-  CacheType type;
+
   int lineSize; // amount of bytes in line
   int linesCount; // amount of lines in cache
   int setsCount; // amount of sets in cache
   int slotsCount; // amount of slots in set
-
-  // TODO: make push and pop method to manipulate data from Core
+  CacheLine *lines; // lines with data
+  int findLessUsedLine(); // returning index of less used line in cache
 };
 
--- cache/cache.cpp
+++ cache/cache.cpp
@@ -10,40 +10,63 @@ constexpr auto LINES_COUNT = 64;
 constexpr auto SETS_COUNT = 4;
 constexpr auto SLOTS_IN_SETS_COUNT = 4;
 
-Cache::Cache(CacheType type)
+Cache::Cache()
 {
-  this->type = type;
-  switch (type)
-  {
-    case L1:
-      this->linesCount = LINES_COUNT;
-      this->lineSize = LINE_SIZE;
-      this->setsCount = SETS_COUNT;
-      this->slotsCount = SLOTS_IN_SETS_COUNT;
-      break;
-    case L2:
-      // Suppose the L2 cache is twice as large as L1
-      this->linesCount = LINES_COUNT * 2;
-      this->lineSize = LINE_SIZE;
-      this->setsCount = SETS_COUNT * 2;
-      this->slotsCount = SLOTS_IN_SETS_COUNT;
-      break;
-    default:
-      this->linesCount = 0;
-      this->lineSize = 0;
-      this->setsCount = 0;
-      this->slotsCount = 0;
-      break;
-  }
+  this->linesCount = LINES_COUNT * 2;
+  this->lineSize = LINE_SIZE;
+  this->setsCount = SETS_COUNT * 2;
+  this->slotsCount = SLOTS_IN_SETS_COUNT;
+  this->lines = new CacheLine[this->linesCount];
 }
 
 std::string Cache::toString() {
   using namespace std;
-  string type = this->type == CacheType::L1 ? "L1" : this->type == CacheType::L2 ? "L2" : "[Unsupported type]";
-  return "Cache " + type + " - Lines count: " + to_string(this->linesCount) + "; Line size: " + to_string(this->lineSize) + ".\n";
+  return "Cache: Lines count: " + to_string(this->linesCount) + "; Line size: " + to_string(this->lineSize) + ".\n";
 }
 
+CacheLine Cache::pop(int index) {
+  CacheLine result = this->lines[index];
+  delete &this->lines[index];
+  return result;
+}
+
+CacheLine Cache::pop(std::string data) {
+  for (int i = 0; i < this->linesCount; i += 1) {
+    if (this->lines[i].data == data) {
+      CacheLine result = this->lines[i];
+      delete& this->lines[i];
+      return result;
+    }
+  }
+}
+
+int Cache::push(std::string data) {
+  int insertedIndex = -1;
+  for (int i = 0; i < this->linesCount; i += 1) {
+    if (&this->lines[i]) {
+      CacheLine line = CacheLine(std::to_string(i / this->setsCount), data);
+      this->lines[i] = line;
+    }
+  }
+}
+
+int Cache::findLessUsedLine() {
+  int minIndex = -1;
+  int minCount = INFINITY;
+  for (int i = 0; i < this->linesCount; i += 1) {
+    if (*&this->lines[i].count < minCount) {
+      minCount = *&this->lines[i].count;
+      minIndex = i;
+    }
+  }
+  return minIndex;
+}
+
+void Cache::clear() {
+  delete this->lines;
+}
 
 Cache::~Cache()
 {
+  delete this->lines;
 }
